f00416c2ffa4d1e22782e1e588b19aecae8a2879
Einsteijun <lordhamedjunior10@gmail.com>
2024-10-03 19:15:33 +0000

    Update and rename ai.js to ask.js

---
+++ scripts/cmds/ask.js
@@ -0,0 +1,70 @@
+const axios = require('axios');
+
+const fonts = {
+
+    mathsans: {
+        a: "𝖺", b: "𝖻", c: "𝖼", d: "𝖽", e: "𝖾", f: "𝖿", g: "𝗀", h: "𝗁", i: "𝗂",
+    j: "𝗃", k: "𝗄", l: "𝗅", m: "𝗆", n: "𝗇", o: "𝗈", p: "𝗉", q: "𝗊", r: "𝗋",
+    s: "𝗌", t: "𝗍", u: "𝗎", v: "𝗏", w: "𝗐", x: "𝗑", y: "𝗒", z: "𝗓",
+    A: "𝐀", B: "𝐁", C: "𝐂", D: "𝐃", E: "𝐄", F: "𝐅", G: "𝐆", H: "𝐇", I: "𝐈",
+    J: "𝐉", K: "𝐊", L: "𝐋", M: "𝗠", N: "𝐍", O: "𝐎", P: "𝐏", Q: "𝐐", R: "𝐑",
+    S: "𝐒", T: "𝐓", U: "𝐔", V: "𝐕", W: "𝐖", X: "𝐗", Y: "𝐘", Z: "𝐙",1: "𝟭", 2: "𝟮", 3: "𝟯", 4: "𝟰", 5: "𝟱", 6: "𝟲", 7: "𝟳", 8: "𝟴", 9: "𝟵", 0: "𝟬"
+    }
+};
+const rolePlay = "quand tu répond à cette question ajoutes des emojis convenable :\n\n";
+
+const Prefixes = [
+  'optimus',
+  'ai',
+  'prime',
+];
+
+module.exports = {
+  config: {
+    name: "ask",
+    version: 1.0,
+    author: "Aesther",
+    longDescription: "AI",
+    category: "ai",
+    guide: {
+      en: "{p} questions",
+    },
+  },
+  onStart: async function () {},
+  onChat: async function ({ api, event, args, message }) {
+    try {
+
+      const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
+      if (!prefix) {
+        return; // Invalid prefix, ignore the command
+      }
+      const prompt = event.body.substring(prefix.length).trim();
+      if (!prompt) {
+      const prompt = event.body.substring(prefix.length).trim();
+api.setMessageReaction("📚", event.messageID, () => {}, true);
+      if (!prompt) {
+        await message.reply("📚");
+        return;
+      }
+        return;
+      }
+      const senderID = event.senderID;
+      const senderInfo = await api.getUserInfo([senderID]);
+      const senderName = senderInfo[senderID].name;
+      const response = await axios.get(`https://sandipbaruwal.onrender.com/gemini?prompt=${encodeURIComponent(rolePlay + prompt)}`);
+      const answer = `[📕] 𝗢𝗣𝗧𝗜𝗠𝗨𝗦 𝗣𝗥𝗜𝗠𝗘\n\n${response.data.answer} \n[📚]········································⬚`;
+api.setMessageReaction("📕", event.messageID, () => {}, true);
+
+      //apply const font to each letter in the answer
+      let formattedAnswer = "";
+      for (let letter of answer) {
+        formattedAnswer += letter in fonts.mathsans ? fonts.mathsans[letter] : letter;
+      }
+
+      await message.reply(formattedAnswer);
+
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  }
+};

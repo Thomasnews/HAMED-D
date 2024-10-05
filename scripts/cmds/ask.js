4d17308c95e71b32369c83813646d84d5871857a
Thomasnews <detectivedee346@gmail.com>
2024-10-05 08:42:36 +0000

    Update ask.js

--- scripts/cmds/ask.js
+++ scripts/cmds/ask.js
@@ -6,17 +6,17 @@ const fonts = {
         a: "𝖺", b: "𝖻", c: "𝖼", d: "𝖽", e: "𝖾", f: "𝖿", g: "𝗀", h: "𝗁", i: "𝗂",
     j: "𝗃", k: "𝗄", l: "𝗅", m: "𝗆", n: "𝗇", o: "𝗈", p: "𝗉", q: "𝗊", r: "𝗋",
     s: "𝗌", t: "𝗍", u: "𝗎", v: "𝗏", w: "𝗐", x: "𝗑", y: "𝗒", z: "𝗓",
-    A: "𝐀", B: "𝐁", C: "𝐂", D: "𝐃", E: "𝐄", F: "𝐅", G: "𝐆", H: "𝐇", I: "𝐈",
-    J: "𝐉", K: "𝐊", L: "𝐋", M: "𝗠", N: "𝐍", O: "𝐎", P: "𝐏", Q: "𝐐", R: "𝐑",
-    S: "𝐒", T: "𝐓", U: "𝐔", V: "𝐕", W: "𝐖", X: "𝐗", Y: "𝐘", Z: "𝐙",1: "𝟭", 2: "𝟮", 3: "𝟯", 4: "𝟰", 5: "𝟱", 6: "𝟲", 7: "𝟳", 8: "𝟴", 9: "𝟵", 0: "𝟬"
+    A: "𝔸", B: "𝔹", C: "ℂ", D: "𝔻", E: "𝔼", F: "𝔽", G: "𝔾", H: "ℍ", I: "𝕀",
+    J: "𝕁", K: "𝕂", L: "𝕃", M: "𝕄", N: "ℕ", O: "𝕆", P: "ℙ", Q: "ℚ", R: "ℝ",
+    S: "𝕊", T: "𝕋", U: "𝕌", V: "𝕍", W: "𝕎", X: "𝕏", Y: "𝕐", Z: "ℤ",1: "𝟭", 2: "𝟮", 3: "𝟯", 4: "𝟰", 5: "𝟱", 6: "𝟲", 7: "𝟳", 8: "𝟴", 9: "𝟵", 0: "𝟬"
     }
 };
 const rolePlay = "quand tu répond à cette question ajoutes des emojis convenable :\n\n";
 
 const Prefixes = [
-  'Dee',
+  'jay',
   'ai',
-  'detective',
+  'walker',
 ];
 
 module.exports = {
@@ -41,9 +41,9 @@ module.exports = {
       const prompt = event.body.substring(prefix.length).trim();
       if (!prompt) {
       const prompt = event.body.substring(prefix.length).trim();
-api.setMessageReaction("📚", event.messageID, () => {}, true);
+api.setMessageReaction("💬", event.messageID, () => {}, true);
       if (!prompt) {
-        await message.reply("📚");
+        await message.reply("𝗵𝗲𝗹𝗹𝗼");
         return;
       }
         return;
@@ -52,8 +52,8 @@ api.setMessageReaction("📚", event.messageID, () => {}, true);
       const senderInfo = await api.getUserInfo([senderID]);
       const senderName = senderInfo[senderID].name;
       const response = await axios.get(`https://sandipbaruwal.onrender.com/gemini?prompt=${encodeURIComponent(rolePlay + prompt)}`);
-      const answer = `[📕] 𝗗𝗘𝗧𝗘𝗖𝗧𝗜𝗩𝗘 𝗗𝗘𝗘\n\n${response.data.answer} \n[📚]········································⬚`;
-api.setMessageReaction("📕", event.messageID, () => {}, true);
+      const answer = `[🥷] 𝗝𝗔𝗬 𝗪𝗔𝗟𝗞𝗘𝗥\n\n${response.data.answer} \n[🌐]········································⬚`;
+api.setMessageReaction("🥷", event.messageID, () => {}, true);
 
       //apply const font to each letter in the answer
       let formattedAnswer = "";
